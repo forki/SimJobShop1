c536ae6b5e8527a2dd0d0e54cb1693e73761f024
lukasimir <lukasimir@hotmail.com>
2017-04-11 09:51:09 +0200

    Sketching out the generic simulation model

---
+++ src/SimJobShop/GenericApproach.fsx
@@ -0,0 +1,85 @@
+﻿type CommandAction<'entity, 'location> = 
+    | Create of 'entity
+    | MoveToWaitlist of 'entity * 'location
+    | MoveToInputBuffer of 'entity * 'location
+    | MoveToOutputBuffer of 'entity * 'location
+    | StartProcess of 'entity * 'location
+    | EndProcess of 'entity * 'location
+    | TakeFromWaitlist of 'location
+
+type EventFact<'time, 'entity, 'location> = 
+    | Created of 'time * 'entity
+    | EnteredWaitlist of 'time * 'entity * 'location
+    | LeftWaitlist of 'time * 'entity * 'location
+    | EnteredInputBuffer of 'time * 'entity * 'location
+    | LeftInputBuffer of 'time * 'entity * 'location
+    | EnteredOutputBuffer of 'time * 'entity * 'location
+    | LeftOutputBuffer of 'time * 'entity * 'location
+    | StartedProcess of 'time * 'entity * 'location
+    | EndedProcess of 'time * 'entity * 'location
+
+type Event<'time, 'entity, 'location> = 
+    { Time : 'time
+      (** TODO: Do we need this too?
+     ModelId : Model Id 
+**)
+      EventFact : EventFact<'time, 'entity, 'location> }
+
+// When an event happened there are commands being issued!
+let CommandsFromEvent = 
+    function 
+    | Created(time, entity) -> 1
+    | EnteredWaitlist(time, entity, location) -> 2
+    | LeftWaitlist(time, entity, location) -> 3
+    | EnteredInputBuffer(time, entity, location) -> 4
+    | LeftInputBuffer(time, entity, location) -> 5
+    | EnteredOutputBuffer(time, entity, location) -> 6
+    | LeftOutputBuffer(time, entity, location) -> 7
+    | StartedProcess(time, entity, location) -> 8
+    | EndedProcess(time, entity, location) -> 9
+
+type Id<'a> = 
+    | Id of uint64
+
+type Location<'entity, 'buffer, 'waitlist> = 
+    { Id : Location<'entity, 'buffer, 'waitlist> Id
+      Capacity : int
+      Waitlist : 'waitlist
+      InputBuffer : 'buffer
+      OutputBuffer : 'buffer }
+
+/// A generic waitlist for entities that uses a comparer to rank the entities and 
+type Waitlist<'entity> = 
+    { Validator : 'entity -> bool
+      Comparer : 'entity -> 'entity -> int
+      Stack : 'entity list }
+
+module Waitlist = 
+    let Create validate compare = 
+        { Validator = validate
+          Comparer = compare
+          Stack = [] }
+(**
+    let Push : 'entity * 'entity Waitlist -> 'entity Waitlist
+    let Pop : 'entity Waitlist -> ('entity * 'entity Waitlist) option
+**)
+
+
+
+(**
+module Location = 
+    let Create id capacity inputBufferCapacity ouputBufferCapacity =
+        if capacity < 1 then
+            None
+        elif inputBufferCapacity < 0 then
+            None
+        elif ouputBufferCapacity < 0 then
+            None
+        else
+            { Id = id
+              Capacity = capacity
+              Waitlist = Waitlist.Create
+              InputBuffer = Buffer.Create inputBufferCapacity
+              OutputBuffer = Buffer.Create ouputBufferCapacity }
+            |> Some
+**)
--- src/SimJobShop/Script.fsx
+++ src/SimJobShop/Script.fsx
@@ -31,7 +31,7 @@ let printJob (job : JobProvider.Row) =
 tasks.Rows |> Seq.iter printTask
 jobs.Rows |> Seq.iter printJob
 
-type Task = 
+type SomeTask = 
     { MachineId : int
       CapacityNeeded : int }
 
@@ -39,3 +39,115 @@ let tasklist =
     [ for task in tasks.Rows -> 
           { MachineId = task.MachineId
             CapacityNeeded = task.CapacityNeeded } ]
+
+
+
+
+[<Measure>]
+type min
+
+type 'a Id = Id of uint64
+
+/// A buffer is a FiFo queue with a limited capacity
+type 'entity Buffer = 
+    { Capacity : int
+      Queue : 'entity System.Collections.Generic.Queue }
+
+/// A location is a non-moving place of action with limited capacity, an input buffer and a waitlist of incoming entities.
+type 'entity Location = 
+    { Id : 'entity Location Id
+      Capacity : int
+      InputBuffer : 'entity Buffer
+      Waitlist : 'entity list }
+
+type 'entity Task = 
+    { MachineId : 'entity Location Id
+      ProcessingTime : float<min> }
+
+/// A job represents a product to be manufactured in the job shop according to a ordered list of tasks
+type Job = 
+    { Id : Job Id
+      Tasks : Job Task list }
+
+type Machine = Job Location
+
+type Shopfloor = 
+    | Shopfloor of Map<Machine Id, Machine>
+
+type JobSequence = 
+    | JobSequence of Job list
+
+type Model = 
+    { Shopfloor : Shopfloor
+      JobSequence : JobSequence }
+
+
+module Buffer = 
+    let Create capacity = 
+        { Capacity = capacity
+          Queue = new System.Collections.Generic.Queue<'a>() }
+    
+    let HasCapacity (buffer : 'a Buffer) = buffer.Queue.Count < buffer.Capacity
+
+    let HasEntities (buffer : 'a Buffer) = buffer.Queue.Count > 0
+
+    let IsEmpty (buffer : 'a Buffer) = HasEntities buffer |> not
+    
+    let Enqueue entity buffer = 
+        if HasCapacity buffer then
+            let mutable queue = buffer.Queue
+            queue.Enqueue entity
+            Some { buffer with Queue = queue }
+        else None
+    
+    let Dequeue buffer =
+        if HasEntities buffer then
+            let mutable queue = buffer.Queue
+            let entity = queue.Dequeue()
+            Some (entity, { buffer with Queue = queue })
+        else None
+
+
+module Location = 
+    let Create id capacity iputBufferCapacity = 
+        if capacity < 1 then
+            None
+        elif iputBufferCapacity < 0 then
+            None
+        else
+            { Id = id
+              Capacity = capacity
+              InputBuffer = Buffer.Create iputBufferCapacity
+              Waitlist = [] }
+            |> Some
+
+
+
+
+
+
+
+
+[<CustomComparison; CustomEquality>]
+type Person = 
+    { Name : string; Age : int }
+
+    override x.Equals yobj =  
+        match yobj with 
+        | :? Person as y -> (x.Name = y.Name) && (x.Age = y.Age)
+        | _ -> false
+    
+    override x.GetHashCode () =
+        hash x
+
+    interface System.IComparable with
+        member x.CompareTo yobj = 
+            match yobj with
+            | :? Person as y -> compare x.Age y.Age
+            | _ -> invalidArg "yobj" "cannot compare value of different types" 
+
+            
+let a = {Name="a";Age=1}
+let b = {Name="b";Age=2}
+let b2 = {Name="b";Age=2}
+b2 <= b
