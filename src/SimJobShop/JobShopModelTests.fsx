3433b2e859b50cb9b45a4c113b7bbc3dcfbb2e45
lukasimir <lukasimir@hotmail.com>
2017-06-10 11:50:53 +0200

    successful tests :)

--- src/SimJobShop/JobShopModelTests.fsx
+++ src/SimJobShop/JobShopModelTests.fsx
@@ -29,6 +29,7 @@ let t2 = JobShopData.makeTask (m2, 0u, TimeSpan.FromHours(2.0), FiniteCapacity 1
 let p1, d1 = JobShopData.makeProduct ([t1;t2], 2.5m, 250u) d0
 let p2, d2 = JobShopData.makeProduct ([t2;t1], 3.0m, 120u) d1
 
+
 // some jobs
 let data = 
     d2
@@ -36,7 +37,7 @@ let data =
     |> JobShopData.makeJob (p1, DateTime.Today, DateTime.Today.AddDays(2.0)) |> snd
 
 // write to files
-JobShopData.writeDataToFiles """C:\Temp\testSim""" data
+//JobShopData.writeDataToFiles """C:\Temp\testSim""" data
 
 
 // ======================================
@@ -48,16 +49,16 @@ let step sim = Simulation.evolveSim saveEvent log sim
 
 let initial = initSimulation data
 
-let sim1 = step initial
-let sim2 = step sim1
-let sim3 = step sim2
-let sim4 = step sim3
-let sim5 = step sim4
-let sim6 = step sim5
-let sim7 = step sim6
-let sim8 = step sim7
-let sim9 = step sim8
-let sim10 = step sim9
+//let sim1 = step initial
+//let sim2 = step sim1
+//let sim3 = step sim2
+//let sim4 = step sim3
+//let sim5 = step sim4
+//let sim6 = step sim5
+//let sim7 = step sim6
+//let sim8 = step sim7
+//let sim9 = step sim8
+//let sim10 = step sim9
 
 let final = Simulation.run saveEvent log initial
 
@@ -66,27 +67,48 @@ let final = Simulation.run saveEvent log initial
 
 (*******************
 
-PROBLEM:
 
-SelectedEntityFromWaitlist for the second entity is performed before
-StartedProcess happened for the first entity:
-
-
-Event '{Time = 30.05.2017 00:00:00;
+Event '{Time = 10.06.2017 00:00:00;
  Fact = CreatedEntityForJob (Id 1UL);}' generated 1 commands
-Event '{Time = 30.05.2017 00:00:00;
+Event '{Time = 10.06.2017 00:00:00;
  Fact = CreatedEntityForJob (Id 2UL);}' generated 1 commands
-Event '{Time = 30.05.2017 00:00:00;
+Event '{Time = 10.06.2017 00:00:00;
  Fact = EnteredWaitlist (Id 1UL,Id 1UL);}' generated 1 commands
-Event '{Time = 30.05.2017 00:00:00;
+Event '{Time = 10.06.2017 00:00:00;
  Fact = EnteredWaitlist (Id 2UL,Id 1UL);}' generated 1 commands
-Event '{Time = 30.05.2017 00:00:00;
- Fact = SelectedEntityFromWaitlist (Id 1UL,Id 1UL);}' generated 1 commands
-Event '{Time = 30.05.2017 00:00:00;
- Fact = SelectedEntityFromWaitlist (Id 2UL,Id 1UL);}' generated 1 commands
-Event '{Time = 30.05.2017 00:00:00;
+Event '{Time = 10.06.2017 00:00:00;
+ Fact = SelectedEntityFromWaitlist (Id 1UL,Id 1UL);}' generated 0 commands
+Event '{Time = 10.06.2017 00:00:00;
  Fact = StartedProcess (Id 1UL,Id 1UL);}' generated 1 commands
-System.Exception: Inconsistency => Not enough capacity available: location id = Id 1UL, capacity available = FiniteCapacity 0u, entity id = Id 2UL, capacity needed = FiniteCapacity 1u
+Event '{Time = 10.06.2017 01:00:00;
+ Fact = EndedProcess (Id 1UL,Id 1UL);}' generated 2 commands
+Event '{Time = 10.06.2017 01:00:00;
+ Fact = EnteredWaitlist (Id 1UL,Id 2UL);}' generated 1 commands
+Event '{Time = 10.06.2017 01:00:00;
+ Fact = SelectedEntityFromWaitlist (Id 2UL,Id 1UL);}' generated 0 commands
+Event '{Time = 10.06.2017 01:00:00;
+ Fact = StartedProcess (Id 2UL,Id 1UL);}' generated 1 commands
+Event '{Time = 10.06.2017 01:00:00;
+ Fact = SelectedEntityFromWaitlist (Id 1UL,Id 2UL);}' generated 0 commands
+Event '{Time = 10.06.2017 01:00:00;
+ Fact = StartedProcess (Id 1UL,Id 2UL);}' generated 1 commands
+Event '{Time = 10.06.2017 02:00:00;
+ Fact = EndedProcess (Id 2UL,Id 1UL);}' generated 2 commands
+Event '{Time = 10.06.2017 02:00:00;
+ Fact = EnteredWaitlist (Id 2UL,Id 2UL);}' generated 1 commands
+Event '{Time = 10.06.2017 03:00:00;
+ Fact = EndedProcess (Id 1UL,Id 2UL);}' generated 2 commands
+Event '{Time = 10.06.2017 03:00:00;
+ Fact = AnnihilatedEntity (Id 1UL);}' generated 0 commands
+Event '{Time = 10.06.2017 03:00:00;
+ Fact = SelectedEntityFromWaitlist (Id 2UL,Id 2UL);}' generated 0 commands
+Event '{Time = 10.06.2017 03:00:00;
+ Fact = StartedProcess (Id 2UL,Id 2UL);}' generated 1 commands
+Event '{Time = 10.06.2017 05:00:00;
+ Fact = EndedProcess (Id 2UL,Id 2UL);}' generated 2 commands
+Event '{Time = 10.06.2017 05:00:00;
+ Fact = AnnihilatedEntity (Id 2UL);}' generated 0 commands
+Simulaten terminated: Schedule is empty
 
 
 
